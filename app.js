0cc4905fee2f2ea0ff98eb26a4b3a43bb29b4963
Omer Farooq <itzomer123@gmail.com>
2024-12-07 15:07:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+// var myName;
+// myName = "Omer Farooq";
+// myName = "Abdullah"
+// var welcomesir = "Welcome sir"
+// alert(myName + welcomesir);
+
+// var number1 ="50";
+// var number2 =100;
+// var number3 =200;
+// alert(number1+number2+number3)
+
+// var myNum =100+400
+// alert (myNum)
+
+
+// var firstNumber = 10
+// var secondNumber = 2
+// alert(firstNumber + secondNumber)
+
+// var num = 10
+// alert(num+1)
+// alert(num++) post increment
+// alert(--num) pre increment
+
+// prompt
+
+var userAnswer = prompt(Enter your age);
+if(userAnswer === "18" ){
+    alert(You are eligible)
+}
+else{
+    (you are not eligible)
+}
+
+
