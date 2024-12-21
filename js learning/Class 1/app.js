959068ea86985558acf6eecd848af932c99dd6bb
Omer Farooq <188629533+Omer166@users.noreply.github.com>
2024-12-21 15:07:00 +0500

    Add files via upload

---
+++ js learning/Class 1/app.js
@@ -0,0 +1,205 @@
+
+
+// var myName;
+// myName = "Omer Farooq";
+// myName = "Abdullah"
+// var welcomesir = "Welcome sir"
+// alert(myName + welcomesir);
+
+// var number1 ="50";
+// var number2 =100;
+// var number3 =200;
+// alert(number1+number2+number3)
+
+// var myNum =100+400
+// alert (myNum)
+
+
+// var firstNumber = 10
+// var secondNumber = 2
+// alert(firstNumber + secondNumber)
+
+// var num = 10
+// alert(num+1)
+// alert(num++) post increment
+// alert(--num) pre increment
+
+// prompt
+
+// var userAnswer = prompt(Enter your age);
+// if(userAnswer === "18" ){
+//     alert(You are eligible)
+// }
+// else{
+//     (you are not eligible)
+// }
+// class 3
+// chapter 17
+// console.log
+// pop to remove last 
+// shift to remove 0 F
+
+// var fruits=["apple","banana","grapes","peach"]
+// console.log("my Fruits:",fruits)
+// fruits.splice(1,2,abc,def)
+
+
+
+// loop
+
+
+
+// var Mynum = 4;
+// for(var i = 3;i<=8; i++ ){console.log(Mynum,"x",i,"=",Mynum*i);}
+// console.log(LOOP );
+
+// class 4
+
+// BOOLIAN
+
+
+
+// var firstNames = ["abu bakar", "umer", "usman", "ali", "maaz"]
+// var laststNames = ["abdullah", "ahmed", "danial", ]
+
+
+// for (var i = 0; i < 5; i++) 
+// {
+//     for (var j = 0; j < 3; j++) {
+//         console.log(firstNames[ i ] + " " + laststNames[j]);
+//     }
+//   console.log("loop is complete")
+// }
+
+
+
+
+// class6
+// task
+
+
+
+
+// var studentmarks=[40,98,55,90,75];
+// for(var i =0;i<studentmarks.length; i++ );
+
+// console.log("total is average")
+
+// var studentmarksan = [40, 98, 55, 90, 75];
+
+// var sum = 0;
+// for (var i = 0; i < studentmarksan.length; i++) {
+//     sum += studentmarksan[i];
+// }
+
+
+// var average = sum / studentmarksan.length;
+
+
+// console.log("The average of the marks is: " + average);
+
+
+
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0)
+//     {console.log(i,"fizzbuzz");
+//     }
+//     else if(i % 3 === 0)
+//     {console.log(i,"fizz");
+//   }
+//   else if(i % 5 === 0)
+//     {console.log(i,"buzz");
+//   }
+// }
+
+
+
+function greetUser(){
+var userName=prompt("Enter your Name");
+alert("welcome" + userName);
+}
+console.log("this is line 1");
+console.log("this is line 2")
+console.log("this is line 3")
+console.log("this is line 4")
+console.log("this is line 5")
+greetUser();
+console.log("this is line 6")
+console.log("this is line 7")
+console.log("this is line 8")
+greetUser();
+console.log("this is line 9")
+console.log("this is line 10")
+greetUser();
+console.log("this is line 1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
